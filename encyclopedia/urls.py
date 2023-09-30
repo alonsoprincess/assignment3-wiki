b89da2909e4fe4f77f6e11a7f57e2115d9343d25
chezmarcbrown <marc@chezbrown.org>
2023-09-30 01:43:17 -0800

    Update README.md

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:entryTitle>", views.entry, name="entry"),
+    path("search/", views.search, name="search"),
+    path("newPage/", views.newPage, name="newPage"),
+    path("edit/", views.edit, name="edit"),
+    path("savePage/", views.savePage, name="savePage"),
+    path("randomize/", views.randomize, name="randomize")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,96 @@
+import random
+from markdown2 import Markdown
 from django.shortcuts import render
+from django import forms
 
 from . import util
 
+def mdConverter(entryTitle):
+    content = util.get_entry(entryTitle)
+    md = Markdown()
+    if content == None:
+        return None
+    else:
+        return md.convert(content)
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def entry(request, entryTitle):
+    entryPage = mdConverter(entryTitle)
+    if entryPage == None:
+        return render(request, "encyclopedia/error.html", {
+            "message" : "This does not exist. Yet..."
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entryTitle" : entryTitle,
+            "entry" : entryPage
+        })
+
+def search(request):
+    if request.method == "POST":
+        search_entry = request.POST['q']
+        value = mdConverter(search_entry)
+        if (value is not None):
+            return render(request, "encyclopedia/entry.html", {
+                "entryTitle" : search_entry,
+                "entry": value
+            })
+        else:
+            closeMatch = []
+            for entry in util.list_entries():
+                if search_entry.lower() in entry.lower():
+                    closeMatch.append(entry)
+            return render(request, "encyclopedia/search.html", {
+                "entryTitle" : search_entry,
+                "closeMatch" : closeMatch
+            })
+        
+def newPage(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/newPage.html")
+    else:
+        entryTitle = request.POST['title']
+        entry = request.POST['content']
+        if (util.get_entry(entryTitle) is not None):
+            return render(request, "encyclopedia/error.html", {
+                "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(entryTitle, entry)
+            entry = mdConverter(entryTitle)
+            return render(request, "encyclopedia/entry.html", {
+                "entryTitle": entryTitle,
+                "entry": entry
+            })
+        
+def edit(request):
+    if request.method == "POST":
+        entryTitle = request.POST['entryTitle']
+        entry = util.get_entry(entryTitle)
+        return render(request, "encyclopedia/edit.html", {
+            "entryTitle": entryTitle,
+            "entry": entry
+        })
+    
+def savePage(request):
+    if request.method == "POST":
+        entryTitle = request.POST['entryTitle']
+        entry = request.POST['entry']
+        util.save_entry(entryTitle, entry)
+        entry = mdConverter(entryTitle)
+        return render(request, "encyclopedia/entry.html", {
+            "entryTitle": entryTitle,
+            "entry": entry
+        })
+    
+def randomize(request):
+    randomEntryTitle = random.choice(util.list_entries())
+    randomEntry = mdConverter(randomEntryTitle)
+    return render(request, "encyclopedia/entry.html", {
+        "entryTitle": randomEntryTitle,
+        "entry": randomEntry
+    })
